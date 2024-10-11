99d21c5a7711781c49cab71995d9dcad1d32d7af
Al-arashi <khaled.jalal.alarashi@gmail.com>
2024-10-11 13:38:56 +0300

    Update functions.js

--- Health Center/js/functions.js
+++ Health Center/js/functions.js
@@ -33,11 +33,41 @@ function showNextSlide() {
 
 setInterval(showNextSlide, 4000);
 
-
 const typed = new Typed(".multiple-text", {
-  strings: [" مركز قوة الحركة للتأهيل الطبي "," خدمات على أعلى مستوى "," من خدماتنا العلاج النفسي ", "من خدماتنا العلاج الوظيفي","و أكثر ..." ],
+  strings: [
+    " مركز قوة الحركة للتأهيل الطبي ",
+    " خدمات على أعلى مستوى ",
+    " من خدماتنا العلاج النفسي ",
+    "من خدماتنا العلاج الوظيفي",
+    "و أكثر ...",
+  ],
   typeSpeed: 100,
   backSpeed: 100,
   backDelay: 1000,
   loop: true,
+});
+
+var divs = document.getElementsByClassName("input-container");
+
+for (var i = 0; i < divs.length; i++) {
+  divs[i].addEventListener("click", function (event) {
+    // Prevent the click event from bubbling up to the document
+    event.stopPropagation();
+
+    // Reset the outline for all containers
+    for (var j = 0; j < divs.length; j++) {
+      divs[j].classList.remove("outline"); // Remove outline class
+    }
+
+    // Add outline class to the clicked container
+    this.classList.add("outline");
+  });
+}
+
+// Add click event listener to the document
+document.addEventListener("click", function () {
+  // Reset the outline for all input-container elements
+  for (var j = 0; j < divs.length; j++) {
+    divs[j].classList.remove("outline"); // Remove outline class
+  }
 });
